1192b76d136792cd5684d0d5d2875ffbaa4f6aec
ariry23 <52358805+ariry23@users.noreply.github.com>
2024-08-15 16:58:51 +0300

    init first pull request

---
+++ src/main/java/com/backend/api/model/ErrorDetails.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.backend.api.model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author pcg29
+ */
+class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
